d07e4d73e383f4f2ec07321382c13cbcf9a06c39
Wendy Hsu <wndyhsu@gmail.com>
2014-03-18 11:54:27 -0500

    mark task complete by id

--- lib/task-manager/task.rb
+++ lib/task-manager/task.rb
@@ -2,13 +2,17 @@
 class TM::Task
   #=module TM (in task-manager.rb)
     #class Task end end
-    attr_accessor :description, :project_id
+    attr_accessor :description, :project_id, :task_complete
 
     def initialize(description, priority_num=0, project_id)
     @description=description
     @priority_num=priority_num
     @project_id=project_id
+    @task_complete=[@project_id,"no"]
   end
 
-
+    def complete
+      @task_complete[1]="yes"
+      @task_complete
+    end
 end
--- spec/task_spec.rb
+++ spec/task_spec.rb
@@ -11,6 +11,16 @@ describe 'Task' do
 
     expect(task1.description).to eq("roadtrip")
     expect(task1.project_id).to eq(TM::Project.id)
+  end
+
+  it "can mark task complete, specify by id" do
+    project=TM::Project.new("hello")
+    task1=TM::Task.new("roadtrip","001", TM::Project.id)
+
+    expect(task1.task_complete).to eq([TM::Project.id, "no"])
+
+    expect(task1.complete).to eq([TM::Project.id, "yes"])
 
   end
+
 end
