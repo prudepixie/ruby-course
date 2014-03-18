15525cbd7d12369066acd1cc96f3cea9807a07a8
Wendy Hsu <wndyhsu@gmail.com>
2014-03-18 11:21:02 -0500

    added name and id

--- lib/task-manager/task.rb
+++ lib/task-manager/task.rb
@@ -1,3 +1,6 @@
 
 class TM::Task
+  #=module TM (in task-manager.rb)
+    #class Task end end
+
 end
--- spec/project_spec.rb
+++ spec/project_spec.rb
@@ -1,7 +1,20 @@
 require 'spec_helper'
 
 describe 'Project' do
+
   it "exists" do
     expect(TM::Project).to be_a(Class)
   end
+  it "can create a new project with a name" do
+    project1=TM::Project.new("newtask")
+    expect(project1.name).to eq("newtask")
+  end
+  it "can automatically create and assign unique id" do
+    project1=TM::Project.new("newtask")
+    id=(0...8).map{(65+rand(26)).chr}.join
+
+    expect(TM::Project.id).not_to be_empty
+  end
+
+
 end
