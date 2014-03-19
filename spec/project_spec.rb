3459dccecb4f607e9db959decf472aac0e492a3e
Wendy Hsu <wndyhsu@gmail.com>
2014-03-18 23:07:08 -0500

    create new project with name

--- spec/project_spec.rb
+++ spec/project_spec.rb
@@ -4,4 +4,11 @@ describe 'Project' do
   it "exists" do
     expect(TM::Project).to be_a(Class)
   end
+
+  it "can create new project with name" do
+    xfiles=TM::Project.new("xfiles")
+    expect(xfiles.name).to eq("xfiles")
+  end
+
+
 end
